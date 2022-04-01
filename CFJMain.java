e59612e772218215e39259cdfa7e0f62f115162f
molychin <34560171+molychin@users.noreply.github.com>
2022-04-01 08:16:00 +0800

    Add files via upload

---
+++ CFJMain.java
@@ -0,0 +1,20 @@
+// FrontEnd Plus GUI for JAD
+// DeCompiled : CFJMain.class
+
+import chaos.app.AppController;
+
+public class CFJMain extends AppController {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8430968041414297125L;
+
+	public CFJMain() {
+		super("Chaos for Java");
+	}
+
+	public static void main(String args[]) {
+		new CFJMain();
+	}
+}
